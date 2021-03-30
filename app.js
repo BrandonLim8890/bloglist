e58b9bf101892fcbe5d2091a3204cbcd17b147e8
Brandon Lim <brandonlim8890@gmail.com>
2021-03-31 00:43:33 +0800

    First commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const blogRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+
+// Initialize connection to mongoose server
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true }).then( () => {
+  logger.info('Succesfully connected to MongoDB')
+}).catch(err => logger.error(err))
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogRouter)
+
+app.use(middleware.unknownEndPoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
